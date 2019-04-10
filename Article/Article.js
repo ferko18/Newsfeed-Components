6174d5c09ffb76ed53dd101ea0df16d89846a36d
Firealem Erko <firealemg@yahoo.com>
2019-04-10 18:35:18 -0400

    toggled the button text between expand and collapse when clicked

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
         this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     
@@ -17,7 +17,19 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
 
-  }
+    if (this.expandButton.textContent =='Expand')
+    {
+      this.expandButton.textContent ='Collapse'
+    }
+    else if (this.expandButton.textContent =='Collapse')
+    {
+      this.expandButton.textContent ='Expand'
+    }
+    else{
+      this.expandButton.textContent ='Expand'
+    }
+
+      }
 }
 
 /* START HERE: 
@@ -36,3 +48,4 @@ articles.forEach(articleBuilder);
 function articleBuilder(e){
   const fancyarticle = new Article (e);
 }
+
